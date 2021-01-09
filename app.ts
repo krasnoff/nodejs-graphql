36071a0427a6aa23778b4948c66ca1c7170a2115
kobi krasnoff <krasnoff.kobi@gmail.com>
2021-01-09 13:29:12 +0200

    embed graphql

--- app.ts
+++ app.ts
@@ -1,8 +1,30 @@
 // lib/app.ts
 import express = require('express');
+import { graphqlHTTP } from 'express-graphql';
+import { buildSchema, GraphQLSchema } from 'graphql';
 
 // Create a new express application instance
 const app: express.Application = express();
+
+// Construct a schema, using GraphQL schema language
+const schema: GraphQLSchema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root: any = {
+    hello: () => {
+        return 'Hello world!';
+    },
+};
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
