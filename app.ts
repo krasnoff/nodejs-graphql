dd4e349cbd65f9ef4cdd26ac3d1edbc42d1ad787
Kobi In Office <Krasnoff.kobi@gmail.com>
2021-01-11 20:30:10 +0200

    targil 1 success

--- app.ts
+++ app.ts
@@ -3,21 +3,68 @@ import express = require('express');
 import { graphqlHTTP } from 'express-graphql';
 import { buildSchema, GraphQLSchema } from 'graphql';
 
+const movies = [
+  {
+    name: 'dune 2020',
+    id: 1,
+    description: 'sdfas asdf asdfawdf asdf asdfasdf '
+  },
+  {
+    name: 'superman',
+    id: 2,
+    description: 'sdfas asdf asdfawdf asdf asdfasdf '
+  },
+  {
+    name: 'batman',
+    id: 2,
+    description: 'sdfas asdf asdfawdf asdf asdfasdf '
+  }
+];
+
 // Create a new express application instance
 const app: express.Application = express();
 
 // Construct a schema, using GraphQL schema language
 const schema: GraphQLSchema = buildSchema(`
   type Query {
-    hello: String
+    hello: String,
+    getAllMovies: [movie],
+    getSingleMovie(id: Int!): movie,
+    updateMovie(id: Int!, name: String!): movie
+  }
+
+  type movie {
+    name: String,
+    id: Int,
+    description: String
   }
 `);
+
+const updateMovie = (args: any) => {
+  let movie = movies.find(el => el.id === args.id)
+  if (movie) {
+    movie.name = args.name;
+  }
+  return movie;
+}
+
+const getSingleMovie = (id: any) => {
+  const movie = movies.find(el => el.id === id.id)
+  return movie;
+}
+
+const getAllMovies = () => {
+  return movies;
+}
 
 // The root provides a resolver function for each API endpoint
 var root: any = {
     hello: () => {
         return 'Hello world!';
     },
+    getAllMovies: getAllMovies,
+    getSingleMovie: getSingleMovie,
+    updateMovie: updateMovie
 };
 
 app.use('/graphql', graphqlHTTP({
